7ec831ca24067a4506c29a56d88c41af629fe21b
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2024-06-25 03:03:45 +0400

    pushed new images in section

--- src/components/info/Info.tsx
+++ src/components/info/Info.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import SupportAndInfo from "../../common-components/common-support-Info/SupportAndInfo";
-import infoData from "./info-data.json";
+import infoData from "./info-data";
 const Info: React.FC = () => {
   return (
-    <SupportAndInfo title={infoData.title} text={infoData.text} id={"info"} />
+    <SupportAndInfo
+      title={infoData.title}
+      text={infoData.text}
+      id={"info"}
+      img={infoData.img}
+    />
   );
 };
 
