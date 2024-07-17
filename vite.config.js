64d9e3ca81475f85109ba0df696ccd45fa0527c4
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2024-07-17 23:36:53 +0400

    new weights

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
-  base: "/regallery/",
+  base: "/regallery",
   build: {
     outDir: "dist",
     rollupOptions: {
