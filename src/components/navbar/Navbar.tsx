1cb1e05b40a97aa537d613849ad7ebdd8322e9d2
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2024-12-10 22:20:36 +0400

    without pricing

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -25,7 +25,6 @@ const links: Link[] = [
   },
   { id: 2, title: "Features", href: "#features" },
   // { id: 3, title: "Pricing", href: "#pricing" },
-  { id: 3, title: "Pricing", href: "#pricing" },
   { id: 4, title: "FAQ", href: "#faq" },
   { id: 5, title: "Contact us", href: "#support" },
 
