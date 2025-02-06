63711c5e8c012ae4fa6d400da60506332494baf9
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2025-02-06 23:37:02 +0400

    minor fix

--- src/components/support/support-data.tsx
+++ src/components/support/support-data.tsx
@@ -1,4 +1,3 @@
-
 import supportImg from "../../assets/imgs/support.webp";
 import supportMiddle from "../../assets/imgs/support-middle.webp";
 import supportSmall from "../../assets/imgs/support-small.webp";
@@ -14,13 +13,13 @@ const supportData = {
   sizes:
     "(max-width: 700px) 208px, (min-width: 701px) and (max-width: 1100px) 277px, 416px",
   alt: `Contact ReGallery - Reach out for support, inquiries, or collaborations with our WordPress photo gallery experts.`,
-
-const supportData = {
-  title: "Contact us",
-  text: "In case you have any questions, suggestions, or if you've found a bug, please visit our Support Forum.We are ready to help you 24/7.",
-  lottiesData:
-    "https://lottie.host/324f91ae-c219-4453-a190-1d91478bb090/zsfBp2hrAs.lottie",
-
 };
+// const supportData = {
+//   title: "Contact us",
+//   text: "In case you have any questions, suggestions, or if you've found a bug, please visit our Support Forum.We are ready to help you 24/7.",
+//   lottiesData:
+//     "https://lottie.host/324f91ae-c219-4453-a190-1d91478bb090/zsfBp2hrAs.lottie",
+
+// };
 
 export default supportData;
