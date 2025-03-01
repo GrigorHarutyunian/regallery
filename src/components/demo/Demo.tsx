51ebc7986a6a8a825ee0ae919e2f9ce1b102fb8f
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2025-03-01 22:22:18 +0400

    import Demo component and change her some styles

--- src/components/demo/Demo.tsx
+++ src/components/demo/Demo.tsx
@@ -1,41 +1,54 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./Demo.css";
 import { Container } from "react-bootstrap";
-
-const demoData = ["Mosaic", "Thumbnails", "Cube"];
+import { Row } from "react-bootstrap";
+import { dataDemo } from "./demo-data";
 
 const Demo: React.FC = () => {
-  const [selectedButton, setSelectedButton] = useState<number>(0);
+  // const [selectedButton, setSelectedButton] = useState<number>(0);
 
-  const handleClick = (index: number) => {
-    if (index !== selectedButton) {
-      setSelectedButton(index);
-    }
-  };
-  console.log(1);
+  // const handleClick = (index: number) => {
+  //   if (index !== selectedButton) {
+  //     setSelectedButton(index);
+  //   }
+  // };
+  // console.log(1);
   return (
     <section id="demo">
       <Container>
-        <div className="demo_columns_content">
-          <h1 className="section-text__title-centered ">Demo</h1>
-          <div className="demo_live_conteiner "></div>
-
-          <div className="demo_buttons_rows">
-            {demoData.map((val, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`download-btn btn_demo ${
-                    selectedButton === index ? "" : "btn_demo_selected"
-                  }`}
-                  onClick={() => handleClick(index)}
-                >
-                  {val}
-                </div>
-              );
-            })}
+        <Row>
+          <div className="demo_columns_content">
+            <h1 className="section-text__title-centered ">Demo</h1>
+            <div className="demo_buttons_rows">
+              {dataDemo.map((val, index) => {
+                return (
+                  <div className="demo_button" key={index}>
+                    <svg
+                      style={{ marginBottom: "5px" }}
+                      height={41}
+                      width={41}
+                      id="Outline"
+                      viewBox="0 0 24 24"
+                    >
+                      {val.path}
+                    </svg>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="demo_live_conteiner ">
+              <div
+                id="reacg-root420"
+                className="reacg-gallery reacg-preview"
+                data-options-section="0"
+                data-plugin-version="1.10.0"
+                data-gallery-timestamp=""
+                data-options-timestamp=""
+                data-gallery-id="420"
+              ></div>
+            </div>
           </div>
-        </div>
+        </Row>
       </Container>
     </section>
   );
