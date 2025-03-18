4dfec9fb743926751b3e567ce2b626adc0d4336f
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2025-03-18 14:07:15 +0400

    used usememo for caching version from cotnext

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext, useMemo } from "react";
 import "./Navbar.css";
 import iconc from "../../assets/icons/logo.webp";
 import DownloadBtn from "../buttons/DownoloadBtn/DownloadBtn";
 import SubMenu from "./SubMenu";
 import { viewsDataSubMenu } from "../views/views-data-subMenu";
-
+import WindowWidthContext from "../../contexts/WindowWidthContext";
 interface Link {
   id: number;
   title: string;
@@ -46,9 +46,11 @@ const links: Link[] = [
 ("Get unlimited access to the pre-built templates for less than$2/month.");
 const Navbar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
-
+  const windowWitdth = useContext(WindowWidthContext);
+  const version = useMemo(() => windowWitdth.version, [windowWitdth.version]);
   console.log(menuOpen);
   useEffect(() => {
+    if (version !== "mobile") return;
     if (!menuOpen) {
       document.body.style.overflowY = "auto";
     } else {
@@ -86,6 +88,7 @@ const Navbar: React.FC = () => {
                 title={link.title}
                 items={link.subMenuItems}
                 href={link.href}
+                version={version}
               />
             ) : (
               <a
