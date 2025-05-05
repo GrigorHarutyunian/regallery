b95e6f275eec1964cdfb6e458a91577243262918
Hakob Hakobyan <hak.hakobian@gmail.com>
2025-05-05 23:54:05 +0400

    Add info about AI

--- src/components/pricing/Pricing.tsx
+++ src/components/pricing/Pricing.tsx
@@ -7,8 +7,7 @@ const Pricing: React.FC = () => {
     <section id="pricing">
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div className="section-text_cards">
-          <h2 className="section-text__title-centered ">Pre-built templates</h2>
-
+          <h2 className="section-text__title-centered">Unlock more features</h2>
           <div className="pricing-cards">
             {pricingData.map((val: PricingDTO) => {
               return (
