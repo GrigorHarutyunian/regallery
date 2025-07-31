d298925f3f5920ccb87036e5bde6c209b414a73a
Hakob Hakobyan <hak.hakobian@gmail.com>
2025-07-31 18:10:10 +0400

    Signed-off-by: Hakob Hakobyan <hak.hakobian@gmail.com>

--- src/components/pricing/PricingCard.tsx
+++ src/components/pricing/PricingCard.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import PricingDTO from "../../types/PricingDTO";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Tooltip } from "@mui/material";
+import { useProVersionActivatorModal } from "../../contexts/ProVersionActivatorModalContext";
+import CustomButton from "../../common-components/custom-button/CustomButton";
 
 const PricingCard: React.FC<PricingDTO> = ({
   price,
@@ -19,6 +21,7 @@ const PricingCard: React.FC<PricingDTO> = ({
     setTooltipText("Copied");
     setTimeout(() => setTooltipText("Copy"), 2000);
   };
+  const { handleOpenModal } = useProVersionActivatorModal();
 
   return (
     <div className="pricing-card">
@@ -59,40 +62,12 @@ const PricingCard: React.FC<PricingDTO> = ({
         </>
       ) : (
         <>
-          <a
-            style={{ position: "relative" }}
-            href={`mailto: regalleryteam@gmail.com?subject=I’m%20interested%20in%20upgrading%20to%20the%20${title}%20plan&body=I’m%20interested%20in%20upgrading%20my%20plan.%20Please%20reach%20out%20to%20me%20with%20the%20next%20steps.%20Thank%20you.`}
-            target="_top"
-          >
-            <div className="pricing-card__btn">
-              EMAIL US FOR {title.toUpperCase()} PLAN
-            </div>
-          </a>
-          <div className="pricing-card__btn_copy">
-            <span>
-              Or copy regalleryteam@gmail.com{" "}
-              <CopyToClipboard
-                text={"regalleryteam@gmail.com"}
-                onCopy={handleCopy}
-              >
-                <Tooltip title={tooltipText} placement="top">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    id="Layer_1"
-                    data-name="Layer 1"
-                    viewBox="0 0 24 24"
-                    width="12"
-                    height="12"
-                  >
-                    <path
-                      fill={"#2540cc"}
-                      d="m19,0h-6c-2.757,0-5,2.243-5,5v6c0,2.757,2.243,5,5,5h6c2.757,0,5-2.243,5-5v-6c0-2.757-2.243-5-5-5Zm3,11c0,1.654-1.346,3-3,3h-6c-1.654,0-3-1.346-3-3v-6c0-1.654,1.346-3,3-3h6c1.654,0,3,1.346,3,3v6Zm-6,8c0,2.757-2.243,5-5,5h-6c-2.757,0-5-2.243-5-5v-6c0-2.757,2.243-5,5-5,.553,0,1,.448,1,1s-.447,1-1,1c-1.654,0-3,1.346-3,3v6c0,1.654,1.346,3,3,3h6c1.654,0,3-1.346,3-3,0-.552.447-1,1-1s1,.448,1,1Z"
-                    />
-                  </svg>
-                </Tooltip>
-              </CopyToClipboard>
-            </span>
-          </div>
+        <CustomButton
+                          handleClick={handleOpenModal}
+                          className={"custom-button"}
+                        >
+                          {"FREE TRIAL"}
+                        </CustomButton>
         </>
       )}
 
