fd9fc0c3b3c39c3f19fa2215183543e9d6839a55
Hakob Hakobyan <hak.hakobian@gmail.com>
2026-06-30 23:54:48 +0400

    changed youtube video

--- src/components/modals/VideoModal/VideoModal.tsx
+++ src/components/modals/VideoModal/VideoModal.tsx
@@ -46,8 +46,8 @@ const VideoModal: React.FC<VideoModalProps> = ({ open, handleClose }) => {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/RldoAkceHTQ?si=HCCyqe5X3tgs211B&autoplay=1&rel=0"
-              title="Photo Gallery Re gallery"
+              src="https://www.youtube.com/embed/hPJzRNCz8VQ?si=HCCyqe5X3tgs211B&autoplay=1&rel=0"
+              title="Photo Gallery"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
